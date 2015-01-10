f6c227c9268c3cbd78b4918feca72c6c47506753
MarcoRmz <marcormz19@gmail.com>
2015-01-09 19:08:22 -0600

    added dog class

---
+++ manBestFriend/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  manBestFriend
+//
+//  Created by Marco Ramirez on 1/9/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ manBestFriend/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  manBestFriend
+//
+//  Created by Marco Ramirez on 1/9/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
