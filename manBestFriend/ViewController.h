be8222bd0cd983058c023b4a2fc43463dbb5263b
MarcoRmz <marcormz19@gmail.com>
2015-01-09 19:03:17 -0600

    initial commit

---
+++ manBestFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  manBestFriend
+//
+//  Created by Marco Ramirez on 1/9/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
