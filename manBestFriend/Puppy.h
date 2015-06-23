5376bddf58bc3897978626e1eaad2a43a1d92fcc
MarcoRmz <marcormz19@gmail.com>
2015-06-22 19:19:17 -0500

    added class puppy

---
+++ manBestFriend/Puppy.h
@@ -0,0 +1,13 @@
+//
+//  Puppy.h
+//  manBestFriend
+//
+//  Created by Marco Ramirez on 6/22/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import "Dog.h"
+
+@interface Puppy : Dog
+
+@end
---
+++ manBestFriend/Puppy.m
@@ -0,0 +1,13 @@
+//
+//  Puppy.m
+//  manBestFriend
+//
+//  Created by Marco Ramirez on 6/22/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import "Puppy.h"
+
+@implementation Puppy
+
+@end
